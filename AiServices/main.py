11365b9516e6700cdc8d926d9153444177b88dd1
Aasher-3689 <aasher36890@gmail.com>
2026-07-17 19:29:54 +0500

    AiServices: Initialized main.py

---
+++ AiServices/main.py
@@ -0,0 +1,11 @@
+# Imports
+from fastapi import FastAPI
+from app.routes.similarity import router as similarity_router
+
+
+# Creating app
+app = FastAPI(title="Anti Plagiarism Suite & Academic Integrity Suite")
+
+
+# Adding routes
+app.include_router(similarity_router)
